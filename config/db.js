9644f48802113152d4e1f8ed5cd72a1c9e162658
Abhishek Patel <patelabhishek11012001@gmail.com>
2023-11-30 00:46:13 +0530

    eigth commit

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(String(process.env.MONGO_URI), {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true,
