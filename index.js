fd825131fcdd8d0f9c125009cd912e732035ab92
Abhishek Patel <patelabhishek11012001@gmail.com>
2023-12-23 13:41:48 +0530

    twelth-3 commit

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 const server = app.listen(PORT, () => {
   console.log(`server started at http://localhost:${PORT}`);
