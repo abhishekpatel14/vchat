9d5fc6f0d58b66b76377fe78d3cbc951b96ff40f
Abhishek Patel <patelabhishek11012001@gmail.com>
2023-12-23 13:26:02 +0530

    twelth-1 commit

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: true,
+    origin: "*",
     // credentials: true,
   },
 });
